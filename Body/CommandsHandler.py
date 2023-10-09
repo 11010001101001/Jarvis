967dfe8c5a9ff2d0e75239343a4996a6f8d288ce
Ярослав Куприянов <upakkomi@gmail.com>
2023-10-09 13:51:00 +0300

    wait command updated

--- Body/CommandsHandler.py
+++ Body/CommandsHandler.py
@@ -267,7 +267,7 @@ class CommandsHandler(Tracer):
 
     @wrapper
     def wait(self):
-        print('режим ожидания активирован на 30 минут 💤')
+        self.log('режим ожидания активирован на 30 минут 💤')
         old_time = time.time()
         ten_min = 600
         twenty_min = ten_min * 2
